c40fb1c0c3a2633064237585fa41e544b229187b
Harshavardhan Kadiyala <devkhv129@zeus.ece.ubc.ca>
2017-10-30 12:41:38 -0700

    added new things

--- master/install.sh
+++ master/install.sh
@@ -4,3 +4,13 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+sudo kubeadm init --apiserver-advertise-address=192.168.77.10 --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl get pods --all-namespaces
+kubectl get nodes
+
